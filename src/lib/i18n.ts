8338eeea9c6b6d059f65146646f59fd64de91574
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2024-12-14 14:22:45 +0100

    feat(route): define new route structure with placeholder pages to navigate to

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,4 +1,27 @@
 import { createI18n } from "@inlang/paraglide-sveltekit";
 import * as runtime from "$lib/paraglide/runtime.js";
 
-export const i18n = createI18n(runtime);
+export const i18n = createI18n(runtime, {
+	pathnames: {
+		"/der-gentleman": {
+			de: "/der-gentleman",
+			en: "/the-gentleman",
+		},
+		"/der-entwickler": {
+			de: "/der-entwickler",
+			en: "/the-developer",
+		},
+		"/der-mentor": {
+			de: "/der-mentor",
+			en: "/the-mentor",
+		},
+		"/die-prinzipien": {
+			de: "/die-prinzipien",
+			en: "/the-principles",
+		},
+		"/kontakt": {
+			de: "/kontakt",
+			en: "/contact",
+		},
+	},
+});
---
+++ src/routes/der-entwickler/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Der Entwickler</h1>
