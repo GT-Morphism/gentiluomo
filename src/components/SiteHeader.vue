b3995aaa5334dda504b4574f32eb9a7fbcabc15b
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-01-17 15:10:00 +0100

    feat(header): add header with path based styling

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,13 +1,74 @@
-<template>
-  <header>
-    <a href="/">LOGO</a>
+<script setup lang="ts">
+  interface Props {
+    currentPath: string;
+  }
 
-    <nav>
-      <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/blog">Blog</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
+  const { currentPath = "/" } = defineProps<Props>();
+</script>
+
+<template>
+  <header
+    class="bg-surface-950 after:from-primary-800 after:via-primary-500 after:to-primary-800 relative py-4 after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 after:bg-gradient-to-r after:content-['']"
+  >
+    <div class="wrapper flex items-center justify-between">
+      <a href="/">LOGO</a>
+
+      <nav class="text-step-sm" aria-label="Hauptnavigation">
+        <ul class="gap-x-m flex items-center">
+          <li>
+            <a
+              href="/kurs"
+              class="p-3xs rounded-md"
+              :class="[
+                {
+                  'bg-primary-500 text-surface-950':
+                    currentPath.includes('kurs'),
+                },
+                {
+                  'hover:outline-primary-500 hover:bg-primary-500/20 focus-visible:bg-primary-500/20 hover:outline':
+                    !currentPath.includes('kurs'),
+                },
+              ]"
+              >Kurs</a
+            >
+          </li>
+          <li>
+            <a
+              href="/blog"
+              class="p-3xs rounded-md"
+              :class="[
+                {
+                  'bg-primary-500 text-surface-950':
+                    currentPath.includes('blog'),
+                },
+                {
+                  'hover:outline-primary-500 hover:bg-primary-500/20 focus-visible:bg-primary-500/20 hover:outline':
+                    !currentPath.includes('blog'),
+                },
+              ]"
+              >Blog</a
+            >
+          </li>
+          <li>
+            <a
+              href="/kontakt"
+              class="p-3xs rounded-md"
+              :class="[
+                {
+                  'bg-primary-500 text-surface-950':
+                    currentPath.includes('kontakt'),
+                },
+                {
+                  'hover:outline-primary-500 hover:bg-primary-500/20 focus-visible:bg-primary-500/20 hover:outline':
+                    !currentPath.includes('kontakt'),
+                },
+              ]"
+              >Kontakt</a
+            >
+          </li>
+          <li><a href="#">Discord</a></li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
