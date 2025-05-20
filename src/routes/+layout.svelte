c90147dbbffdd0c1342c466d64e6f7451c8b9414
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-05-20 22:07:15 +0200

    chore: add view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,15 @@
 <script lang="ts">
+  import { onNavigate } from "$app/navigation";
   const { children } = $props();
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 </script>
 {@render children()}
