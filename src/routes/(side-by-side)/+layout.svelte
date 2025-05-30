05063b4cf9c22c9bdf34781de6ec1ecf83db9c14
Giuseppe Turturiello <giuseppe-pio@hotmail.de>
2025-05-30 14:47:52 +0200

    refactor: use data attributes conventions for side-by-side layout

--- src/routes/(side-by-side)/+layout.svelte
+++ src/routes/(side-by-side)/+layout.svelte
@@ -4,18 +4,18 @@
   const { children } = $props();
 </script>
 
-<div class="side-by-side wrapper">
-  <div data-config-side-by-side="sidebar">
+<div data-layout="side-by-side" class="wrapper">
+  <div data-sidebyside-child="sidebar">
     <DonPageNavigation />
   </div>
-  <main data-config-side-by-side="content">
+  <main class="flow prose" data-sidebyside-child="content">
     {@render children()}
   </main>
 </div>
 
 <style>
-  @layer components {
-    .side-by-side {
+  @layer layouts {
+    [data-layout="side-by-side"] {
       --_content-column-span: 1;
       --_content-row-start: 2;
       --_content-column-start: 1;
@@ -30,35 +30,35 @@
       */
       grid-template-columns: repeat(auto-fit, minmax(min(100%, 200px), 1fr));
       column-gap: var(--spacing-s-m); /* clamp(1rem, 0.75rem + 1.25vw, 1.875rem) */
+    }
 
-      [data-config-side-by-side="sidebar"] {
-        display: none;
-        @container side-by-side (inline-size >= calc(3 * 200px + 200px + 3 * clamp(1rem, 0.75rem + 1.25vw, 1.875rem))) {
-          display: block;
-        }
+    [data-sidebyside-child="sidebar"] {
+      display: none;
+      @container side-by-side (inline-size >= calc(3 * 200px + 200px + 3 * clamp(1rem, 0.75rem + 1.25vw, 1.875rem))) {
+        display: block;
+      }
+    }
+
+    [data-sidebyside-child="content"] {
+      grid-column-start: var(--_content-column-start);
+      grid-column-end: span var(--_content-column-span);
+      grid-row-start: var(--_content-row-start);
+      min-block-size: 200vb;
+      background-color: oklch(from var(--color-primary-800) l c h / 0.2);
+      border-radius: var(--border-radius-md);
+      border: 2px solid var(--color-primary-800);
+
+      padding-block: var(--spacing-xs-s);
+      padding-inline: var(--spacing-2xs-xs);
+
+      @container side-by-side (inline-size >= calc(3 * 200px + 200px + 3 * clamp(1rem, 0.75rem + 1.25vw, 1.875rem))) {
+        --_content-row-start: 1;
+        --_content-column-start: 2;
+        --_content-column-span: 3;
       }
 
-      [data-config-side-by-side="content"] {
-        grid-column-start: var(--_content-column-start);
-        grid-column-end: span var(--_content-column-span);
-        grid-row-start: var(--_content-row-start);
-        min-block-size: 200vb;
-        background-color: oklch(from var(--color-primary-800) l c h / 0.2);
-        border-radius: var(--border-radius-md);
-        border: 2px solid var(--color-primary-800);
-
-        padding-block: var(--spacing-xs-s);
-        padding-inline: var(--spacing-2xs-xs);
-
-        @container side-by-side (inline-size >= calc(3 * 200px + 200px + 3 * clamp(1rem, 0.75rem + 1.25vw, 1.875rem))) {
-          --_content-row-start: 1;
-          --_content-column-start: 2;
-          --_content-column-span: 3;
-        }
-
-        @container side-by-side (inline-size >= calc(4 * 200px + 200px + 4 * clamp(1rem, 0.75rem + 1.25vw, 1.875rem))) {
-          --_content-column-span: 4;
-        }
+      @container side-by-side (inline-size >= calc(4 * 200px + 200px + 4 * clamp(1rem, 0.75rem + 1.25vw, 1.875rem))) {
+        --_content-column-span: 4;
       }
     }
   }
